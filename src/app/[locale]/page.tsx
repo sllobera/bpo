e4e29076dab4b50e8943967e52899dc582c654a7
sllobera <sebastian@llobera.com.au>
2024-09-22 09:16:39 +1000

    ii

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,33 @@
+'use client'
 import Button from './components/Button'
+import { useState } from 'react'
+import {
+  Label,
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions
+} from '@headlessui/react'
+import {
+  CheckIcon,
+  ChevronUpDownIcon,
+  QuestionMarkCircleIcon
+} from '@heroicons/react/20/solid'
 
+const people = [
+  { id: 1, name: 'Wade Cooper' },
+  { id: 2, name: 'Arlene Mccoy' },
+  { id: 3, name: 'Devon Webb' },
+  { id: 4, name: 'Tom Cook' },
+  { id: 5, name: 'Tanya Fox' },
+  { id: 6, name: 'Hellen Schmidt' },
+  { id: 7, name: 'Caroline Schultz' },
+  { id: 8, name: 'Mason Heaney' },
+  { id: 9, name: 'Claudie Smitham' },
+  { id: 10, name: 'Emil Schaefer' }
+]
 export default function DashboardPage() {
+  const [selected, setSelected] = useState(people[3])
   return (
     <div className='back min-h-96 md:block md:bg-cover md:bg-left-top lg:block xl:block 2xl:block'>
       <div className='max-w-custom90 mx-auto w-full'>
@@ -15,6 +42,61 @@ export default function DashboardPage() {
                 a range of Australia&lsquo;s Outsourcing Companies{' '}
                 <span className='font-bold'>for over 20 years</span>
               </p>
+              <div className='space-y-4'>
+                <Listbox value={selected} onChange={setSelected}>
+                  <div className='relative mb-5 mt-2'>
+                    <ListboxButton className='font-ProximaNova text-paragraphColor flex h-12 w-full cursor-pointer appearance-none items-center justify-between whitespace-normal break-words rounded border bg-white p-2 focus:outline-none'>
+                      <span className='font-ProximaNova block truncate'>
+                        {selected.name}
+                      </span>
+                      <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+                        <ChevronUpDownIcon
+                          aria-hidden='true'
+                          className='h-5 w-5 text-gray-400'
+                        />
+                      </span>
+                    </ListboxButton>
+
+                    <ListboxOptions
+                      transition
+                      className='absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm'
+                    >
+                      {people.map(person => (
+                        <ListboxOption
+                          key={person.id}
+                          value={person}
+                          className='group relative cursor-default select-none py-2 pl-3 pr-9 text-left text-gray-900 data-[focus]:bg-gray-600 data-[focus]:text-white'
+                        >
+                          <span className='font-ProximaNova block truncate font-normal group-data-[selected]:font-semibold'>
+                            {person.name}
+                          </span>
+
+                          <span className='absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden'>
+                            <CheckIcon aria-hidden='true' className='h-5 w-5' />
+                          </span>
+                        </ListboxOption>
+                      ))}
+                    </ListboxOptions>
+                  </div>
+                </Listbox>
+                <div>
+                  <div className='relative mb-5 mt-2 rounded-md shadow-sm'>
+                    <input
+                      id='account-number'
+                      name='account-number'
+                      type='text'
+                      placeholder='000-00-0000'
+                      className='font-ProximaNova text-paragraphColor h-12 w-full rounded-b rounded-t border p-2 focus:outline-none'
+                    />
+                    <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3'>
+                      <QuestionMarkCircleIcon
+                        aria-hidden='true'
+                        className='h-5 w-5 text-gray-400'
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
               <button className='font-Toroka rounded-custom-tl-br inline-flex w-auto items-center justify-center rounded-[15px_5px_15px_5px] bg-white p-[5px] px-2 py-[14px] text-[20px] font-normal leading-4 leading-normal text-primary transition-colors duration-300 sm:px-8 sm:text-2xl'>
                 <p></p>START COMPARING
               </button>
