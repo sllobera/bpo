6a35523bf9e88c2afb682d89452edb5a06973bf9
sllobera <sebastian@llobera.com.au>
2024-09-21 17:39:48 +1000

    opi

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -12,30 +12,24 @@ interface Props {
 export const Header: FC<Props> = ({ locale }) => {
   const t = useTranslations('')
   return (
-    <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-2'>
-      <Link lang={locale} href='/'>
-        <div className='flex flex-row items-center'>
-          <div className='mb-2 h-8 w-14'></div>
-          <strong className='mx-2 select-none'>Template</strong>
+    <>
+      <div className='text-color-white mx-auto flex max-w-full flex-row items-center justify-between bg-primary p-2'>
+        <Link lang={locale} href='/'>
+          <div className='flex flex-row items-center'>
+            <div className='mb-2 h-8 w-14'></div>
+            <strong className='mx-2 select-none'>Template</strong>
+          </div>
+        </Link>
+        <div className='flex flex-row items-center gap-3'>
+          <nav className='mr-10 inline-flex gap-5'>
+            <Link lang={locale} href={`/about`}>
+              {t('About')}
+            </Link>
+            <a href=''>{t('Support')}</a>
+            <a href=''>{t('Other')}</a>
+          </nav>
         </div>
-      </Link>
-      <div className='flex flex-row items-center gap-3'>
-        <nav className='mr-10 inline-flex gap-5'>
-          <Link lang={locale} href={`/about`}>
-            {t('About')}
-          </Link>
-          <a href=''>{t('Support')}</a>
-          <a href=''>{t('Other')}</a>
-        </nav>
-        {/* <ThemeSwitch />*/}
-        <LangSwitcher />
-        <a
-          href='https://github.com/yahyaparvar/nextjs-template'
-          target='_blank'
-        >
-          <div className='size-8'></div>
-        </a>
       </div>
-    </div>
+    </>
   )
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,93 +1,62 @@
-import { useTranslations } from 'next-intl'
 import Button from './components/Button'
 
 export default function DashboardPage() {
-  const t = useTranslations('')
   return (
-    <div>
-      {/*} <section className='flex flex-col items-center justify-center py-24'>
-        <h1 className='text-center text-7xl font-extrabold leading-tight'>
-          {t('An')}{' '}
-          <span className='bg-span-bg bg-clip-text text-transparent'>
-            {t('Booster')}
-          </span>
-          <br />
-          {t('to_Your_NextJS_Apps')}
-        </h1>
-        <div className='my-6 px-20 text-center text-2xl text-text-secondary'>
-          {t(
-            'An_approachable_performant_and_versatile_boilerplate_for_building_SSR_applications'
-          )}
-        </div>
-        <div className='mt-4 flex flex-row gap-4'>
-          <a
-            href='https://github.com/new?template_name=nextjs-template&template_owner=yahyaparvar'
-            target='_blank'
-          >
-            <Button rounded size='large'>
-              {t('Use_Template')}
-            </Button>
-          </a>
-          <a
-            href='https://github.com/yahyaparvar/nextjs-template'
-            target='_blank'
-          >
-            <Button rounded size='large' variant='secondary'>
-              {t('Learn_More')}
-            </Button>
-          </a>
-        </div>
-      </section>*/}
-      <section className='bg-background-secondary'>
-        <div className='relative isolate overflow-hidden bg-gray-900'>
-          <div className='px-6 py-24 sm:px-6 sm:py-32 lg:px-8'>
-            <div className='mx-auto max-w-2xl text-center'>
-              <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>
-                Boost your productivity.
-                <br />
-                Start using our app today.
-              </h2>
-              <p className='mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300'>
-                Incididunt sint fugiat pariatur cupidatat consectetur sit cillum
-                anim id veniam aliqua proident excepteur commodo do ea.
+    <div className='back min-h-96 md:block md:bg-cover md:bg-left-top lg:block xl:block 2xl:block'>
+      <div className='max-w-custom90 mx-auto w-full'>
+        <div className='relative flex h-full flex-col items-start md:ml-5 md:pt-28 lg:ml-28 lg:pt-20'>
+          <div className='rounded-tl-custom5 rounded-t-md bg-secondary'>
+            <div className='rounded-tl-custom5 rounded-br-custom5 relative w-full max-w-md rounded-t-md bg-primary p-10 text-center shadow-lg'>
+              <h1 className='font-TorokaBold mb-4 text-[40px] leading-9 text-white'>
+                COMPARE OUTSOURCING
+              </h1>
+              <p className='font-ProximaNova mb-4 text-lg leading-6 text-white'>
+                We've been saving Aussies time, effort and money comparing a
+                range of Australia's health funds{' '}
+                <span className='font-bold'>for over 20 years</span>
               </p>
-              <div className='mt-10 flex items-center justify-center gap-x-6'>
-                <a
-                  href='#'
-                  className='rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white'
-                >
-                  Get started
-                </a>
-                <a
-                  href='#'
-                  className='text-sm font-semibold leading-6 text-white'
-                >
-                  Learn more <span aria-hidden='true'>→</span>
-                </a>
-              </div>
+              <button className='font-Toroka rounded-custom-tl-br inline-flex w-auto items-center justify-center rounded-[15px_5px_15px_5px] bg-white p-[5px] px-2 py-[14px] text-[20px] font-normal leading-4 leading-normal text-primary transition-colors duration-300 sm:px-8 sm:text-2xl'>
+                <p></p>START COMPARING
+              </button>
+            </div>
+
+            <div className='flex w-full'>
+              <a
+                href='tel:1800784772'
+                className='flex w-1/2 px-6 py-6 text-white'
+              >
+                <img
+                  alt='Logo'
+                  loading='lazy'
+                  width='24'
+                  height='24'
+                  decoding='async'
+                  data-nimg='1'
+                  className='mr-2'
+                  src='/assets/icon_Phone.svg'
+                />
+                <p className='call-text font-ProximaNovaBold'>
+                  CALL 1800 784 772
+                </p>
+              </a>
+              <div className='mb-[12px] mt-[18px] inline-block w-px bg-gray-500'></div>
+              <button className='flex w-1/2 px-6 py-6 text-white'>
+                <img
+                  alt='Logo'
+                  loading='lazy'
+                  width='24'
+                  height='24'
+                  decoding='async'
+                  data-nimg='1'
+                  className='mr-2'
+                  src='/assets/icon_schedule_call.svg'
+                />
+                <p className='font-ProximaNovaBold'>SCHEDULE A CALL</p>
+              </button>
             </div>
           </div>
-          <svg
-            viewBox='0 0 1024 1024'
-            aria-hidden='true'
-            className='absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]'
-          >
-            <circle
-              r={512}
-              cx={512}
-              cy={512}
-              fill='url(#8d958450-c69f-4251-94bc-4e091a323369)'
-              fillOpacity='0.7'
-            />
-            <defs>
-              <radialGradient id='8d958450-c69f-4251-94bc-4e091a323369'>
-                <stop stopColor='#7775D6' />
-                <stop offset={1} stopColor='#E935C1' />
-              </radialGradient>
-            </defs>
-          </svg>
         </div>
-      </section>
+      </div>
     </div>
   )
 }
